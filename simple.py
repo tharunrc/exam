64ac989150af8d721ecb9e7cbf39d771d42c3b00
tharunrc <109327970+tharunrc@users.noreply.github.com>
2022-08-26 19:58:51 +0530

    Add files via upload

---
+++ simple.py
@@ -0,0 +1,11 @@
+if username == 'Put Your Username Here':#change this
+   print("checking username")
+   time.sleep(0.5)
+   print("username is right")
+   password = input(' type your password: ')
+   if password == 'Put Your Password Here':#change this
+    time.sleep(0.5)
+   else:
+    exit()
+else:
+ exit()
